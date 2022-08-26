2543cc0a876bd0acd63cd9b05589ecc79ea856ff
NJ-AI <72422648+Zhang-NJ-Lab@users.noreply.github.com>
2022-08-26 15:53:49 +0800

    Add files via upload

---
+++ bianli_fetch.sh
@@ -0,0 +1,6 @@
+IFS=$'\n'; for file in $( find -name "*outmol"); do echo -n -e $file '\t' && grep -A 0 "DFT" $file | tail -1;done>2.txt
+
+# -A 0 对应 DFT energy gap
+# -A 1 对应 valence band edge
+# -A 2 对应 conduction band edge
+# -A n 代表后面关键词之后的n行文本
---
+++ fetchVBM.sh
@@ -0,0 +1,6 @@
+IFS=$'\n'; for file in $( find -name "*outmol"); do echo -n -e $file '\t' && grep -A 1 "DFT" $file | tail -1;done>VBM.txt
+
+# -A 0 对应 DFT energy gap
+# -A 1 对应 valence band edge
+# -A 2 对应 conduction band edge
+# -A n 代表后面关键词之后的n行文本
